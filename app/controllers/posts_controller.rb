1980e6d5d846b8f91f6b5ca604d350a4138a3bdf
Steve Schofield <stevebschofield@gmail.com>
2014-01-14 07:36:06 +0000

    Created Post resource and updated index and show views

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+  def index
+  	@posts = Post.all
+  end
+
+  def show
+  	@post = Post.find(params[:id])
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  private
+	def post_params
+		params.required(:post).permit(:title, :body)
+	end
+end
