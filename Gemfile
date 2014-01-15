203bc7ce6bb63c710672420f1b0947414784ae74
Steve Schofield <stevebschofield@gmail.com>
2014-01-15 06:23:38 +0000

    added devise and couldn't resist a little styling

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '4.0.2'
 
 gem 'pg'
 gem 'faker'
+gem 'devise'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Bloccit::Application.routes.draw do
   
+  as :user do 
+    get '/register', to: 'devise/registrations#new', as: :register
+    get '/login', to: 'devise/sessions#new', as: :login
+    get '/logout', to: 'devise/sessions#destroy', as: :logout
+
+  end
+
+  devise_for :users, skip: [:sessions]
+
+  as :user do
+    get "/login" => 'devise/sessions#new', as: :new_user_session
+    post "/login" => 'devise/sessions#create', as: :user_session
+    delete "/logout" => 'devise/sessions#destroy', as: :destroy_user_session
+  end
+  
  resources :posts
 
   match "about" => 'welcome#about', via: :get
