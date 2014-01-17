1ebf933ec0999ec51b6d90305434dd7865bf19aa
Steve Schofield <stevebschofield@gmail.com>
2014-01-17 05:40:30 +0000

    Installed and implemented Redcarpet for Markdown

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'pg'
 gem 'faker'
 gem 'devise'
 gem 'cancan'
+gem 'redcarpet'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,18 @@
 module ApplicationHelper
+  #helper for form errors
   def control_group_tag(errors, &block)
     if errors.any?
       content_tag :div, capture(&block), class: 'control-group form-group error'
     else
       content_tag :div, capture(&block), class: 'control-group form-group'
     end
+  end
+
+  #helper for markdown conversion
+  def markdown(text)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render text).html_safe
   end
 end
