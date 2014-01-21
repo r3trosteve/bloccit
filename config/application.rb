82f03d4839a3c05dba285ce83150a092483067b9
Steve Schofield <stevebschofield@gmail.com>
2014-01-21 12:25:22 +0000

    rolled back change to application config

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,6 @@ Bundler.require(:default, Rails.env)
 module Bloccit
   class Application < Rails::Application
     config.assets.paths << "#{Rails}/vendor/assets/fonts"
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
