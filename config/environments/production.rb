77c061dfe5c603e2ca9acacd045170767e534605
Steve Schofield <stevebschofield@gmail.com>
2014-01-22 06:01:18 +0000

    Implemented Favoriting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ Bloccit::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'r3trobloccit.herokuapp.com' } 
+  config.action_mailer.default_url_options = { :host => 'www.jello.io' } 
 end
