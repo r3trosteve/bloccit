17e77327ee1e4a909508ec0d75d697b87388b106
Steve Schofield <stevebschofield@gmail.com>
2014-01-23 07:05:17 +0000

    created popular posts and users, optimised topics#show with eager loading

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,69 +1,7 @@
 class PostsController < ApplicationController
-  load_and_authorize_resource 
-  skip_load_resource only: [:create] 
 
-  def show
-    @topic = Topic.find(params[:topic_id])
-    authorize! :read, @topic, message: "You need to be signed-in to do that."
-    @post = Post.find(params[:id])
-    @comments = @post.comments
-    @comment = Comment.new
-  end
-
-  def new
-    @topic = Topic.find(params[:topic_id])
-  	@post = Post.new
-    authorize! :create, Post, message: "You need to be a member to create a new post."
-  end
-
-  def create
-  @topic = Topic.find(params[:topic_id])
-  @post = current_user.posts.build(post_params)
-  @post.topic = @topic
-  authorize! :create, @post, message: "You need to be signed up to do that."
-  if @post.save
-    redirect_to [@topic, @post], notice: "Post was saved."
-  else
-    flash[:error] = "There was an error saving the post. Please try again."
-    render :new
-  end
+def index
+	@posts = Post.visible_to(current_user).where("posts.created_at > ?",7.days.ago).paginate(page: params[:page], per_page: 10)
 end
 
-  def edit
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
-    authorize! :edit, @post, message: "You need to own the post to edit it."
-  end
-
-  def update
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
-    authorize! :update, @post, message: "You need to own the post to edit it."
-    if @post.update_attributes(post_params)
-      redirect_to [@topic, @post], notice: "Post was saved"
-    else
-      flash[:error] = "There was an error saving the post. Please try again."
-      render :edit
-    end
-  end
-
-  def destroy
-    @topic = Topic.find(params[:topic_id])
-    @post = Post.find(params[:id])
-
-    title = @post.title
-    authorize! :destroy, @post, message: "You need to own the post to delete it."
-    if @post.destroy
-      flash[:notice] = "\"#{title}\" was deleted successfully."
-      redirect_to @topic
-    else
-      flash[:error] = "There was an error deleting the post."
-      render :show
-    end
-  end  
-
-  private
-	def post_params
-		params.require(:post).permit(:title, :body, :topic, :image)
-	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Bloccit::Application.routes.draw do
   
+  resources :posts, only: [:show, :index]
   resources :topics do
-   resources :posts, except: [:index] do
+   resources :posts, except: [:index], controller: 'topics/posts' do
      resources :comments, only: [:create, :destroy]
      match '/up-vote', to: 'votes#up_vote', as: :up_vote, via: [:get, :post]
      match '/down-vote', to: 'votes#down_vote', as: :down_vote, via: [:get, :post]
@@ -12,7 +13,7 @@ Bloccit::Application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'users/registrations' }
 
   resources :users, only: [:show]
-  
+
   match "about" => 'welcome#about', via: :get
   match "privacy" => 'welcome#privacy', via: :get
   
